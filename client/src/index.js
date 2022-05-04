89f9412fd3c14dcd3ab590331327870cc4429da7
omidkhaksar <mazaherkhaksar1994@gmail.com>
2022-05-04 14:31:35 +0430

    debuging Project and Create stable version

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {createStore,applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
-import { createRoot } from 'react-dom/client';
-import App from './components/App.js';
+
+import App from './components/App';
 import reducers from './reducers';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store =createStore(
-        reducers,
-        composeEnhancer(applyMiddleware(reduxThunk))
-        );
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-        <Provider store={store} >
-                <App tab="home" />
-        </Provider>
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
 );
--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -7,7 +7,6 @@ import {
   DELETE_STREAM
 } from '../actions/types';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:

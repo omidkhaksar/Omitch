8bc9d3bbc1a2bf3378668cc0e679969b985f00db
omidkhaksar <mazaherkhaksar1994@gmail.com>
2022-05-04 16:26:59 +0430

    Make Rmtp Server and Use Flv.js in Client

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,10 +1,11 @@
+
 import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
 const INTIAL_STATE = {
   isSignedIn: null,
   userId: null
 };
-
+// eslint-disable-next-line
 export default (state = INTIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
